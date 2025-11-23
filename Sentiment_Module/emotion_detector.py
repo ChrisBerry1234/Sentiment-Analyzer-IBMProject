c7426936b2506a6ab21166be6e07254cc0c57c47
ChrisBerry1234 <christopherjberry846@gmail.com>
2025-11-22 19:26:43 -0500

    Created logic to sort through json response dict using nested loops to display dominant emotion

--- Sentiment_Module/emotion_detector.py
+++ Sentiment_Module/emotion_detector.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,6 +7,17 @@ def emotion_detector(text_to_analyze):
     myjson = {"raw_document": {"text": text_to_analyze }}
 
     response = requests.post(url, json = myjson, headers = headers)
+    formatted_response = json.loads(response.text)
 
-    return response.text
+    Dominant_score = 1
+    Dominant_emotion = None
 
+    for x in formatted_response['emotionPredictions']:
+        for emotion, score in x['emotion'].items():
+            print({f'"{emotion}": {score}'})
+            if score <= Dominant_score and score >= .90:
+                Dominant_score = score
+                Dominant_emotion = emotion
+    
+    print({f'"Dominant emotion": {Dominant_emotion}: {Dominant_score}'})    
+         
