78a68e987c4202dd9f82ef44c4297abb90dbfcbc
ChrisBerry1234 <christopherjberry846@gmail.com>
2025-11-26 23:59:49 -0500

    Parsed object from server in webscript.js and user inner.html to display object values to UI with styling

--- static/webscript.js
+++ static/webscript.js
@@ -8,7 +8,7 @@ addEventListener('keydown', (e) => {
     if (e.code === "Enter"){
         sendHttpRequest();
     }
-} )
+})
    
 
 function sendHttpRequest() {
@@ -18,13 +18,19 @@ function sendHttpRequest() {
     xml.onreadystatechange = function() {
         if (this.readyState == 4){
             if (this.status == 200){
-                document.getElementById('results').innerHTML = xml.responseText;
+                const respobj = JSON.parse(xml.responseText);
+                //scores is an object within an object so we need to extract it
+                const respobjscores = JSON.stringify(respobj, null, 2);
+                document.getElementById('results').innerHTML = `<p style="color: green;"> For the given text: <span style="font-weight: bold;"> ${respobj.input} </span> <br>
+                The system response is ${respobjscores} <br> 
+                The Dominant Emotion is ${respobj.dominant_emotion}! </p>`
             }
             if (this.status == 400){
                 document.getElementById('results').innerHTML = xml.responseText;
             }
         }
     }
+    document.getElementById("user-text").value = '';
 
     xml.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
     xml.send();
